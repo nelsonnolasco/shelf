e8630a036a070c1e44203987d6776f8a812d2ca4
Nelson de Campos Nolasco <nelsonnolasco@gmail.com>
2025-01-11 21:55:27 -0300

    estrutura spring boot mais maven

---
+++ src/test/java/brcomncn/shelf/ShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package brcomncn.shelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
